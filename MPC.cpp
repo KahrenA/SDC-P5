af92b7e07be65f36994bc25a70f781a24d0281f0
KahrenA <KahrenA@gmail.com>
2017-09-09 20:45:05 -0700

    Add files via upload

--- MPC.cpp
+++ MPC.cpp
@@ -59,7 +59,7 @@ class FG_eval
 		// The part of the cost based on the reference state.
 		for (unsigned int t = 0; t < N; t++) 
 		{
-			fg[0] += 25 * CppAD::pow(vars[cte_start + t], 2);
+			fg[0] += 45 * CppAD::pow(vars[cte_start + t], 2);
 			fg[0] += 1 * CppAD::pow(vars[epsi_start + t], 2);
 			fg[0] += 1 * CppAD::pow(vars[v_start + t] - ref_v, 2);
 		}
@@ -75,7 +75,7 @@ class FG_eval
 		for (unsigned int t = 0; t < N - 2; t++) 
 		{
 			fg[0] += 1 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-			fg[0] += 1 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+			fg[0] += 5 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
 		}
 
 		//----------------------------------------------------------------
